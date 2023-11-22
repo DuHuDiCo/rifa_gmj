30d7d54a6d2d2a2e870bef70f3b1e99c0ecc8ff4
Duvan Diaz <dudicodiaz@gmail.com>
2023-11-22 16:39:28 -0500

    c

--- src/main/java/com/gmjHogar/rifa_bakend/controllers/ParticipanteController.java
+++ src/main/java/com/gmjHogar/rifa_bakend/controllers/ParticipanteController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/participante")
-@CrossOrigin(origins = {"http://localhost:4200/", "http://almacenelectrohogar.com/", "https://pagafacil.almacenelectrohogar.com/", "https://rifa.almacenelectrohogar.com/"})
+@CrossOrigin(origins = {"http://localhost:4200/", "http://almacenelectrohogar.com/", "https://pagafacil.almacenelectrohogar.com/", "https://rifa.almacenelectrohogar.com/", "*"})
 public class ParticipanteController {
     
     
